02cc85e845d4974b7e31ee8658334e8f8dcb0401
Hans Kurniawan <kurniaw.hans@gmail.com>
2017-10-07 15:09:34 +0200

    Car now follows the checkpoint, checkpoint repositions after collision

--- Assets/OnTriggerScript.cs
+++ Assets/OnTriggerScript.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OnTriggerScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public List<Vector3> checkpointList = new List<Vector3>(); // List containing the vector3's of the checkpoint places
+    private int checkpointCounter = 0;
+    // Use this for initialization
+    void Start () {
+        // Adding the checkpoints to the list
+        checkpointList.Add(new Vector3(-60f, -2.74f, -120f));
+        checkpointList.Add(new Vector3(-40f, -2.74f, -150f));
+        checkpointList.Add(new Vector3(-20f, -2.74f, -180f));
+        checkpointList.Add(new Vector3(-5f, -2.74f, -220f));
+        checkpointList.Add(new Vector3(20f, -2.74f, -260));
+        checkpointList.Add(new Vector3(14f, -2.74f, -355));
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,23 +22,25 @@ public class OnTriggerScript : MonoBehaviour {
 	}
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "CarBoxCollider")
+        if (other.gameObject.name == "CheckpointCollisionChecker")
         {
-            Debug.Log("Its the car box collider");
-            
         }
         else
         {
-            Debug.Log("Its something else");
+            return;
         }
-        Debug.Log("Name: " + other.gameObject.ToString());
     }
     void OnTriggerStay(Collider other)
     {
+        if (other.gameObject.name == "CheckpointCollisionChecker") // If there is a collision with the checkpointcollider of the car, it will change it's position
+        {
+            this.transform.position = checkpointList[checkpointCounter];
+            checkpointCounter++;
+      
 
+        }
     }
     void OnTriggerExit(Collider other)
     {
-
     }
 }
