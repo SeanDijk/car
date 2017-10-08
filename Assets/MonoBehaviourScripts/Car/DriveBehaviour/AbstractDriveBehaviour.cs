1deb260888e5bf2cd365b5ec94c906373ca4d72a
Tobi van Westerop <tobivanwesterop@gmail.com>
2017-10-08 15:02:26 +0200

    Implementation of envoirment.
    Implementation of envoirment.

--- Assets/MonoBehaviourScripts/Car/DriveBehaviour/AbstractDriveBehaviour.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using UnityEditor;
-
-public abstract class AbstractDriveBehaviour
-{
-    public abstract void FixedUpdate(Car c, float maxTorqueFw, float maxTorqueBw, float maxTorqueBrake, float maxSteerAngle, AbstractSensorBehaviour[] sensorBehaviours);
-}
