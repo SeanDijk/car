2918a71b326ca5a157e870527afa7b077780ae2f
Sean van Dijk <Seandijk@gmail.com>
2017-10-23 15:37:23 +0200

    Lidar now saves images

--- Assets/SensorBehaviour/LidarDepthMapScript.cs
+++ Assets/SensorBehaviour/LidarDepthMapScript.cs
@@ -1,39 +1,42 @@
 ﻿
 using System;
+using System.IO;
 using UnityEngine;
 using UnityEngine.VR.WSA.WebCam;
 //so that we can see changes we make without having to run the game
 
-[ExecuteInEditMode]
 [System.Serializable]
 public class LidarDepthMapScript : MonoBehaviour
-{
-    PhotoCapture photoCaptureObject = null;
-    Texture2D targetTexture = null;
+{    
     public Material mat;
-    // Use this for initialization
+
+    private ScreenRecorder screenRecorder = null;
+    private Camera myCam = null;
+
+    private string folder = null;
     void Start()
     {
+        //Load up the screenrecorder
+        screenRecorder = (ScreenRecorder) ScriptableObject.CreateInstance(typeof(ScreenRecorder));
+        screenRecorder.captureWidth = 1280;
+        screenRecorder.captureHeight = 720;
+
+        folder = Path.GetFullPath(Application.dataPath + "/..");
+        folder += "/lidar";
+        System.IO.Directory.CreateDirectory(folder);
+
+        ChangeFolder();
+
+        //Load the depthtexture
+        myCam = GetComponent<Camera>();
         GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
 
-
+        InvokeRepeating("LidarAction", 1f, .25f);
     }
 
-    // Update is called once per frame
-    int prev = 0;
     void Update()
     {
-        var newNum = Convert.ToInt32(Time.fixedTime);
-        if (newNum > prev)
-        {
-            prev = newNum;
-            for (int i = 0; i < 4; i++)
-            {
-                ScreenCapture.CaptureScreenshot("test/" + prev + "_" + i+ ".png");
-                transform.RotateAround(transform.position, transform.up, 90f);
-            }
 
-        }
     }
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
@@ -44,5 +47,48 @@ public class LidarDepthMapScript : MonoBehaviour
 
     }
 
+    void CaptureImage()
+    {
+        screenRecorder.CaptureScreenshot(myCam);
+    }
 
+
+    int rotateCounter = 0;
+    int folderCounter = 0;
+    // Rotates the camera 90 degrees
+    void RotateCamera90Degrees()
+    {
+        transform.RotateAround(transform.position, transform.up, 90f);
+        rotateCounter++;
+
+        if (rotateCounter > 0 && rotateCounter % 4 == 0)
+        {
+            rotateCounter = 0;
+            folderCounter++;
+        }
+        ChangeFolder();
+
+    }
+
+    void ChangeFolder()
+    {
+        var tempString = folder + "/" + folderCounter;
+
+        switch (rotateCounter)
+        {
+            case 0: tempString += "/front"; break;
+            case 1: tempString += "/right"; break;
+            case 2: tempString += "/back"; break;
+            case 3: tempString += "/left"; break;
+        }
+
+        System.IO.Directory.CreateDirectory(tempString);
+        screenRecorder.folder = tempString;
+    }
+
+    void LidarAction()
+    {
+        CaptureImage();
+        RotateCamera90Degrees();
+    }
 }
